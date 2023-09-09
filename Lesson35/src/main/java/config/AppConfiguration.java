203e30bc4ce91e4f8f723d7e989c5dad4b1e7207
37544 <brinrf@mail.ru>
2023-09-09 09:29:41 +0300

    lesson 37

--- Lesson35/src/main/java/config/AppConfiguration.java
+++ Lesson35/src/main/java/config/AppConfiguration.java
@@ -6,8 +6,10 @@ import domain.Jockey;
 import domain.Pair;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
+@EnableAspectJAutoProxy
 public class AppConfiguration {
 
     @Bean
