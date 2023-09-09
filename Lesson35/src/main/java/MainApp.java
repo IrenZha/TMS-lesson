2112ea907b493cca3da4c21c40d5770625c5f23b
37544 <brinrf@mail.ru>
2023-09-09 09:14:54 +0300

    lesson 35

--- Lesson35/src/main/java/MainApp.java
+++ Lesson35/src/main/java/MainApp.java
@@ -1,21 +1,13 @@
-import domain.Client;
-import domain.Horse;
-import domain.Jockey;
-import domain.Pair;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import service.ClientService;
 import service.Hippodrome;
-import service.RunService;
-
-import java.util.List;
 
 public class MainApp {
     public static void main(String[] args) {
        // ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
 
-        ApplicationContext context = new AnnotationConfigApplicationContext("config", "domain", "service");
+        ApplicationContext context = new AnnotationConfigApplicationContext("config", "domain", "service", "aspect");
         Hippodrome hippodrome = context.getBean(Hippodrome.class);
         hippodrome.game();
     }
--- Lesson35/src/main/java/domain/Jockey.java
+++ Lesson35/src/main/java/domain/Jockey.java
@@ -2,7 +2,6 @@ package domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.stereotype.Component;
 
 @Data
 @AllArgsConstructor
