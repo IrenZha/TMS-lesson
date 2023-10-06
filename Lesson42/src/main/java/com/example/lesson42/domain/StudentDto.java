5e9384648e4f19de9b6807589e4e44abe06480c1
37544 <brinrf@mail.ru>
2023-10-06 19:11:58 +0300

    Lesson 43

--- Lesson42/src/main/java/com/example/lesson42/domain/StudentDto.java
+++ Lesson42/src/main/java/com/example/lesson42/domain/StudentDto.java
@@ -1,13 +1,14 @@
 package com.example.lesson42.domain;
 
 import com.example.lesson42.model.Gender;
+import lombok.Data;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.UUID;
-
+@Data
 public class StudentDto {
     private UUID id;
     @NotBlank
@@ -19,51 +20,4 @@ public class StudentDto {
     private Gender gender;
     private Integer number;
 
-    public StudentDto(UUID id, String name, Integer age, Gender gender, Integer number) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-        this.number = number;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
-    }
-
-    public Integer getNumber() {
-        return number;
-    }
-
-    public void setNumber(Integer number) {
-        this.number = number;
-    }
 }
--- Lesson42/src/main/java/com/example/lesson42/model/StudentEntity.java
+++ Lesson42/src/main/java/com/example/lesson42/model/StudentEntity.java
@@ -1,11 +1,13 @@
 package com.example.lesson42.model;
 
+import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
+@Data
 public class StudentEntity {
     @Id
     @GeneratedValue(generator = "UUID")
@@ -17,45 +19,6 @@ public class StudentEntity {
     private Gender gender;
     private Integer number;
     private static int index = 1;
-
-    public StudentEntity() {
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
-    }
-
-    public Integer getNumber() {
-        return number;
-    }
 
     public void setNumber(Integer number) {
         this.number = index;
