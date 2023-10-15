94a613a86ef4ee75aef90bd26dc29bd57cbc1e69
37544 <brinrf@mail.ru>
2023-10-15 14:30:17 +0300

    Lesson 47

--- Lesson46_2/src/main/java/com/example/lesson46_2/config/AppConfig.java
+++ Lesson46_2/src/main/java/com/example/lesson46_2/config/AppConfig.java
@@ -1,16 +1,10 @@
 package com.example.lesson46_2.config;
 
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableFeignClients(basePackages = "com.example.lesson46_2.client")
 public class AppConfig {
 
-//    @Bean
-//    RestTemplate template(){
-//        return new RestTemplate();
-//    }
 }
