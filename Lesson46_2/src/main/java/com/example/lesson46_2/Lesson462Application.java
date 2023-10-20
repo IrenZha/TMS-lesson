fee01bc815492fd1472cb41971ecaf05fe06baba
37544 <brinrf@mail.ru>
2023-10-20 20:17:45 +0300

    Lesson 47

--- Lesson46_2/src/main/java/com/example/lesson46_2/Lesson462Application.java
+++ Lesson46_2/src/main/java/com/example/lesson46_2/Lesson462Application.java
@@ -2,7 +2,11 @@ package com.example.lesson46_2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableFeignClients(basePackages = "com.example.lesson46_2.client")
 @SpringBootApplication
 public class Lesson462Application {
 
